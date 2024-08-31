23f1a4fffab4ab54b81202a0c5beb099af414c79
adamhussien <see069962@gmail.com>
2024-08-31 06:37:41 -1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [open, setopen] = useState(false)
+  const toggole = function(){
+    setopen(!open)
+  }
+ return(
+  <div>
+    <Navbar ontoggle={toggole} open={open}/>
+  </div>
+ )
 }
 
 export default App;
+function Navbar({ontoggle, open}){
+  return (
+    <div className='flex justify-between items-center p-[1rem]  m-[1rem]  '> 
+       
+       <div className='uppercase font-bold text-zinc-100 text-[1.6rem] '>adm</div>
+       <ul className={`  flex justify-between items-center gap-4 navlinks ${open ? "active" : ""}`}>
+         <i  className='uppercase text-[1.2rem] text-[#fff] font-bold hover:text-gray-500 transition ease-out'>home</i>
+         <i  className='uppercase text-[1.2rem] text-[#fff] font-bold hover:text-gray-500 transition ease-out' >about</i>
+         <i  className='uppercase text-[1.2rem] text-[#fff] font-bold hover:text-gray-500 transition ease-out'>service</i>
+         <i className='uppercase text-[1.2rem] text-[#fff] font-bold hover:text-gray-500 transition ease-out' >contact</i>
+       </ul>
+       <div  className='uppercase text-[#ffff] text-[2rem] font-bold toggle' onClick={ontoggle}>{open ? "X" : '☰' }</div>
+    </div>
+   );
+}
+
+
+
+
+
+
+
+
